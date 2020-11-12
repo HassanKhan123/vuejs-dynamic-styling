642c9751cb29b1d0c1ad2be12965062d3c035c0a
HassanKhan123 <hk2602809@gmail.com>
2020-11-12 21:50:57 +0500

    adding classes dynamically

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            boxASelected:false,
-            boxBSelected:false,
-            boxCSelected:false,
-        }
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    boxSelect(box) {
+      if (box === 'A') {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === 'B') {
+        this.boxBSelected = !this.boxBSelected;
+      }
+      if (box === 'C') {
+        this.boxCSelected = !this.boxCSelected;
+      }
     },
-    methods:{
-        boxSelect(box){
-            if(box === 'A'){
-                this.boxASelected=true
-            }
-            else if(box === 'B'){
-                this.boxBSelected=true
-            }
-            if(box === 'C'){
-                this.boxCSelected=true
-            }
-        }
-    }
-})
+  },
+});
 
-app.mount('#styling')
+app.mount('#styling');
